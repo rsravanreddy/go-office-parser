29fefe666471803d59d15e5d53458ea92fb2de9c
sravan rekula <sravan@thinair.com>
2019-03-28 20:00:18 -0700

    update tests and example

--- example.go
+++ example.go
@@ -2,49 +2,79 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"io"
 
+	"github.com/richardlehane/mscfb"
 	"github.com/rsravanreddy/go-office-parser/parser"
 )
 
 func main() {
 
-	var dr io.Reader
-	dr, _ = parser.NewDocxReader("./testdata/demo.docx")
+	{
+		var dr io.ReadCloser
+		dr, _ = parser.NewDocxReader("./testdata/demo.docx")
 
-	buf := make([]byte, 4000)
-	s := ""
-	for {
-		n, err := dr.Read(buf)
-		s += string(buf[:n])
-		if err == io.EOF {
-			break
+		buf := make([]byte, 4000)
+		s := ""
+		for {
+			n, err := dr.Read(buf)
+			s += string(buf[:n])
+			if err == io.EOF {
+				break
+			}
 		}
-	}
-	fmt.Println(s)
+		fmt.Println(len(s))
 
-	dr, _ = parser.NewXlsxReader("./testdata/sample.xlsx")
+		dr.Close()
 
-	buf = make([]byte, 4000)
-	s = ""
-	for {
-		n, err := dr.Read(buf)
-		s += string(buf[:n])
-		if err == io.EOF {
-			break
+		dr, _ = parser.NewXlsxReader("./testdata/sensitive123.xlsx")
+
+		buf = make([]byte, 4000)
+		s = ""
+		for {
+			n, err := dr.Read(buf)
+			s = string(buf[:n])
+			if err == io.EOF {
+				break
+			}
 		}
+		fmt.Println(len(s))
+		dr.Close()
+
+		// res, err := xlsxparser.Parse("./testdata/sample.xlsx")
+		// elapsed := time.Since(start)
+		// log.Printf("parsing took %s", elapsed)
+		// if err == nil {
+		// 	fmt.Println(res)
+		// } else {
+		// 	fmt.Println(err)
+
+		// }
+
+		file, _ := os.Open("testdata/test.doc")
+		defer file.Close()
+		doc, err := mscfb.New(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for entry, err := doc.Next(); err == nil; entry, err = doc.Next() {
+			//buf := make([]byte, 1024)
+			//_, _ := doc.Read(buf)
+			// if i > 0 {
+			// 	fmt.Println("data" + string(buf[:i]))
+			// }
+			fmt.Println(entry.Name)
+		}
+
 	}
-	fmt.Println(s)
 
-	// res, err := xlsxparser.Parse("./testdata/sample.xlsx")
-	// elapsed := time.Since(start)
-	// log.Printf("parsing took %s", elapsed)
-	// if err == nil {
-	// 	fmt.Println(res)
-	// } else {
-	// 	fmt.Println(err)
+	for {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println("sleeping..")
 
-	// }
-
+	}
 }
